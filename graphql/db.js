9cf4ae57fac2ac5aa875c5d825333bf445274d4a
Sung-jun Um <zerodice0@gmail.com>
2021-03-04 22:20:55 +0900

    add Base URLs(LIST_MOVIES_URL, MOVIE_DETAILS_URL, MOVIE_SUGGESTIONS_URL). call http api by using axios.

--- graphql/db.js
+++ graphql/db.js
@@ -1,19 +1,45 @@
-import axios from "axios"
-
-const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+import axios from "axios";
+const BASE_URL = "https://yts-proxy.now.sh/";
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAILS_URL = `${BASE_URL}movie_details.json`;
+const MOVIE_SUGGESTIONS_URL = `${BASE_URL}movie_suggestions.json`;
 
 export const getMovies = async (limit, rating) => {
-  let REQUEST_URL = API_URL
-  if (limit > 0) {
-    REQUEST_URL += `&limit=${limit}`
-  }
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(LIST_MOVIES_URL, {
+    params: {
+      limit,
+      minimum_rating: rating
+    }
+  });
+  return movies;
+};
 
-  if (rating > 0) {
-    REQUEST_URL += `&minimum_rating=${rating}`
-  }
+export const getMovie = async id => {
+  const {
+    data: {
+      data: { movie }
+    }
+  } = await axios(MOVIE_DETAILS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movie;
+};
 
-  const result = await axios(REQUEST_URL)
-  const data = await result.data
-
-  return data.data.movies
-}
+export const getSuggestions = async id => {
+  const {
+    data: {
+      data: { movies }
+    }
+  } = await axios(MOVIE_SUGGESTIONS_URL, {
+    params: {
+      movie_id: id
+    }
+  });
+  return movies;
+};
